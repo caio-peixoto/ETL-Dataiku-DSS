ce5acbc16a1f46b924646bf19ecece137fdcf5d0
Rebecca Pontes Salles <rebeccapsalles@gmail.com>
2019-01-25 10:47:18 -0200

    Update load.dbc.sim.r

--- SIM/load.dbc.sim.r
+++ SIM/load.dbc.sim.r
@@ -1,11 +1,11 @@
-load.dbc.sim <- function(files){
+load.dbc <- function(files){
   require(read.dbc)
   
-  do_from_DBC=data.frame()
+  df_from_DBC=data.frame()
   
   for(f in files){
     # Compute a data frame for the output to write
-    bind_rows( do_from_DBC, read.dbc(f) ) -> do_from_DBC
+    bind_rows( df_from_DBC, read.dbc(f) ) -> df_from_DBC
   }
-  return(do_from_DBC)
+  return(df_from_DBC)
 }
